859aea8ab1758c43b9db049b99a90bc7b5d745f5
saba871 <sabatabatadze40@gmail.com>
2025-05-28 20:19:45 -0700

    commit

---
+++ day 129/classwork/introdaction/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main>
+      <p>hello world</p>
+    </main>
+  )
+}
+
+export default App
---
+++ day 129/homework/second-react/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <main>
+        <h1>Hello world</h1>
+    </main>
+  )
+}
+
+export default App
